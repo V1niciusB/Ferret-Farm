6640ed4a8fc4ce68444b69474553441b5fd5478f
V1niciusB <viniciusbarbosa@usp.br>
2017-03-31 09:26:54 -0300

    Update Aula 2.c

--- Aula 2.c
+++ Aula 2.c
@@ -3,29 +3,29 @@
 #include <windows.h>
 //clock-alarm
 int main(){
-int n, segundo=0, minuto=0, hora=0, aH, aM;
-char p;
+int n, seg=0, min=0, hora=0, aH, aM;
+char k;
 n=1;
 printf("Que horas deseja despertar?\n");
-scanf("%d%c%d", &aH, &p, &aM);
+scanf("%d%c%d", &aH, &k, &aM);
 while(n >0){
-if(segundo == 60){
-    segundo = 0;
-    minuto++;
+if(seg == 60){
+    seg = 0;
+    min++;
 }
-if(minuto == 60){
-    minuto = 0;
+if(min == 60){
+    min = 0;
     hora++;
 }
 if(hora==24){
     hora = 0;
 }
 {
-printf("\r  Hora: %d : %d : %d ", hora, minuto, segundo);
-segundo++;
+printf("\r  Hora: %d : %d : %d ", hora, min, seg);
+seg++;
 sleep(1);// 1 segundo
 }
-if(hora==aH && minuto==aM){
+if(hora==aH && min==aM){
    printf("\n Acorde!");
    break;
 }
