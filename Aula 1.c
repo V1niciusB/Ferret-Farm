ee64366fee7f3d286be74d9b3fe158aee1266435
V1niciusB <viniciusbarbosa@usp.br>
2017-03-31 09:27:59 -0300

    Update Aula 1.c

--- Aula 1.c
+++ Aula 1.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<windows.h>
-//relógio
+#include <windows.h>
+//clock
 int main(){
-int n, segundo=0, minuto=0, hora=0;
+int n, seg=0, min=0, hora=0;
 n=1;
 while(n >0){
-if(segundo == 60){
-    segundo = 0;
-    minuto++;
+if(seg == 60){
+    seg = 0;
+    min++;
 }
-if(minuto == 60){
-    minuto = 0;
+if(min == 60){
+    min = 0;
     hora++;
 }
 if(hora==24){
     hora = 0;
 }
-printf("\r  Hora: %d : %d : %d ", hora, minuto, segundo);
-segundo++;
+printf("\r  Hora: %d : %d : %d ", hora, min, seg);
+seg++;
 sleep(1);// 1 segundo
 }
 
