2c115813dc0b70648e7f3e431790903f0ca02bc5
V1niciusB <viniciusbarbosa@usp.br>
2017-04-07 08:59:44 -0300

    Update Aula 2+.c

--- Aula 2+.c
+++ Aula 2+.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h>
+#include <windows.h> //Para usar o sleep
 //clock-alarm
 int main(){
-int n, seg=0, min=0, hora=0, aH, aM, aS;
-    char k;
+int n, seg=0, min=0, hora=0, aH, aM, aS; //variaveis inteiras
+    char k; //variavel literal
     n=1;
     printf("Que horas deseja despertar?\n");//Hora:Minuto:Segundo
-    scanf("%d%c%d%c%d", &aH, &k, &aM, &k, &aS);
+    scanf("%d%c%d%c%d", &aH, &k, &aM, &k, &aS);// %d=inteiro %c=letra
     while(n >0){
         if(seg == 60){
             seg = 0;
@@ -23,11 +23,11 @@ int n, seg=0, min=0, hora=0, aH, aM, aS;
         {
             printf("\r Hora: %d : %d :%d ", hora, min, seg);
             seg++;
-            sleep(1);//1 segundo
+            sleep(1);//pausa o "seg++" por 1 segundo -> o relogio nao fica turbo
         }
         if(hora==aH && min==aM && seg==aS){
             printf("\nAcorde!");
-            break;
+            break; //para o relógio
         }
     }
     return 0;
